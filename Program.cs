c4603735ced5a243714c3bb7be876cc08dfce712
Said-laasri <saidlaasri3@gmail.com>
2023-03-29 17:38:59 +0000

    Add Eliza file

--- Program.cs
+++ Program.cs
@@ -1,13 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
+﻿using System;
 
-namespace menu
+namespace ElizaIsSilly
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Doctor.Intro());
             
         }
     }
